246f51c488ec6446906e751630f797e5710736c4
bing <bing@area17.com>
2018-04-17 10:33:38 -0400

    Added rollup

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-export { default as ajaxRequest } from './src/ajaxRequest.js';
-export { default as cookieHandler } from './src/cookieHandler.js';
-export { default as copyTextToClipboard } from './src/copyTextToClipboard';
-export { default as debounce } from './src/debounce';
-export { default as escapeString } from './src/escapeString.js';
-export { default as extend } from './src/extend.js';
-export { default as fontLoadObserver } from './src/fontLoadObserver';
-export { default as forEach } from './src/forEach';
-export { default as getCurrentMediaQuery } from './src/getCurrentMediaQuery.js';
-export { default as getIndex } from './src/getIndex.js';
-export { default as getMetaContentByName } from './src/getMetaContentByName.js';
-export { default as getOffset } from './src/getOffset.js';
-export { default as getUrlParameterByName } from './src/getUrlParameterByName.js';
-export { default as jsonpRequest } from './src/jsonpRequest.js';
-export { default as keycodes } from './src/keycodes.js';
-export { default as lazyLoad } from './src/lazyLoad';
-export { default as manageBehaviors } from './src/manageBehaviors.js';
-export { default as messages } from './src/messages.js';
-export { default as objectifyForm } from './src/objectifyForm.js';
-export { default as oritentationChangeFix } from './src/oritentationChangeFix.js';
-export { default as purgeProperties } from './src/purgeProperties.js';
-export { default as queryStringHandler } from './src/queryStringHandler';
-export { default as resized } from './src/resized.js';
-export { default as scrollToY } from './src/scrollToY.js';
-export { default as sendEventToSegmentio } from './src/sendEventToSegmentio.js';
-export { default as setFocusOnTarget } from './src/setFocusOnTarget';
-export { default as triggerCustomEvent } from './src/triggerCustomEvent.js';
+'use strict';
+
+var a17helpers = require('/dist/prod.js');
+
+module.exports = a17helpers;
---
+++ src/index.js
@@ -0,0 +1,61 @@
+import ajaxRequest from './src/utility/ajaxRequest.js';
+import cookieHandler from './src/utility/cookieHandler.js';
+import copyTextToClipboard from './src/utility/copyTextToClipboard';
+import debounce from './src/utility/debounce';
+import escapeString from './src/utility/escapeString.js';
+import extend from './src/utility/extend.js';
+import fontLoadObserver from './src/utility/fontLoadObserver';
+import forEach from './src/utility/forEach';
+import getCurrentMediaQuery from './src/utility/getCurrentMediaQuery.js';
+import getIndex from './src/utility/getIndex.js';
+import getMetaContentByName from './src/utility/getMetaContentByName.js';
+import getOffset from './src/utility/getOffset.js';
+import getUrlParameterByName from './src/utility/getUrlParameterByName.js';
+import jsonpRequest from './src/utility/jsonpRequest.js';
+import keycodes from './src/utility/keycodes.js';
+import lazyLoad from './src/utility/lazyLoad';
+import manageBehaviors from './src/behavior/manageBehaviors.js';
+import messages from './src/utility/messages.js';
+import objectifyForm from './src/utility/objectifyForm.js';
+import oritentationChangeFix from './src/utility/oritentationChangeFix.js';
+import purgeProperties from './src/utility/purgeProperties.js';
+import queryStringHandler from './src/utility/queryStringHandler';
+import resized from './src/utility/resized.js';
+import scrollToY from './src/utility/scrollToY.js';
+import sendEventToSegmentio from './src/utility/sendEventToSegmentio.js';
+import setFocusOnTarget from './src/utility/setFocusOnTarget';
+import triggerCustomEvent from './src/utility/triggerCustomEvent.js';
+
+
+
+const a17helpers = {
+  ajaxRequest,
+  cookieHandler,
+  copyTextToClipboard,
+  debounce,
+  escapeString,
+  extend,
+  fontLoadObserver,
+  forEach,
+  getCurrentMediaQuery,
+  getIndex,
+  getMetaContentByName,
+  getOffset,
+  getUrlParameterByName,
+  jsonpRequest,
+  keycodes,
+  lazyLoad,
+  manageBehaviors,
+  messages,
+  objectifyForm,
+  oritentationChangeFix,
+  purgeProperties,
+  queryStringHandler,
+  resized,
+  scrollToY,
+  sendEventToSegmentio,
+  setFocusOnTarget,
+  triggerCustomEvent
+};
+
+export default a17helpers;
