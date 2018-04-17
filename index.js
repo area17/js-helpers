8ab1a54865496c131175c9fca65dd92647afb3f1
bing <bing@area17.com>
2018-04-17 11:03:59 -0400

    Finish setup of rollup

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
 
-var a17helpers = require('/dist/prod.js');
+var a17helpers = require('./dist/prod');
 
 module.exports = a17helpers;
--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,16 @@
-import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   input: 'src/index.js',
   output: {
-    file: '/dist/prod.js',
+    file: 'dist/prod.js',
     format: 'cjs'
   },
   plugins: [
     resolve(),
+    commonjs(),
     babel({
       exclude: 'node_modules/**' // only transpile our source code
     })
