50633dc67567f998724ad21cb3f5b8809f3c6b1c
bing <bing@area17.com>
2018-05-04 12:20:52 -0400

    Generate new dist files

--- dist/prod.js
+++ dist/prod.js
@@ -606,6 +606,64 @@ var getUrlParameterByName = function getUrlParameterByName(name, url) {
   return qsObj[name] !== undefined ? qsObj[name] : undefined;
 };
 
+var isBreakpoint = function isBreakpoint(bp) {
+  // Doc: https://code.area17.com/a17/a17-helpers/wikis/isBreakpoint
+
+  // bail if no breakpoint is passed
+  if (!bp) {
+    console.error('You need to pass a breakpoint name!');
+    return false;
+  }
+
+  // we only want to look for a specific modifier and make sure it is at the end of the string
+  var pattern = new RegExp('\\+$|\\-$');
+
+  // bps must be in order from smallest to largest
+  var bps = ['xsmall', 'small', 'medium', 'large', 'xlarge', 'xxlarge'];
+
+  // override the breakpoints if the option is set on the global A17 object
+  if (window.A17 && window.A17.breakpoints) {
+    if (Array.isArray(window.A17.breakpoints)) {
+      bps = window.A17.breakpoints;
+    } else {
+      console.warn('A17.breakpoints should be an array. Using defaults.');
+    }
+  }
+
+  // store current breakpoint in use
+  var currentBp = getCurrentMediaQuery();
+
+  // store the index of the current breakpoint
+  var currentBpIndex = bps.indexOf(currentBp);
+
+  // check to see if bp has a + or - modifier
+  var hasModifier = pattern.exec(bp);
+
+  // store modifier value
+  var modifier = hasModifier ? hasModifier[0] : false;
+
+  // store the trimmed breakpoint name if a modifier exists, if not, store the full queried breakpoint name
+  var bpName = hasModifier ? bp.slice(0, -1) : bp;
+
+  // store the index of the queried breakpoint
+  var bpIndex = bps.indexOf(bpName);
+
+  // let people know if the breakpoint name is unrecognized
+  if (bpIndex < 0) {
+    console.warn('Unrecognized breakpoint. Supported breakpoints are: ' + bps.join(', '));
+    return false;
+  }
+
+  // compare the modifier with the index of the current breakpoint in the bps array with the index of the queried breakpoint.
+  // if no modifier is set, compare the queried breakpoint name with the current breakpoint name
+  if (modifier === '+' && currentBpIndex >= bpIndex || modifier === '-' && currentBpIndex <= bpIndex || !modifier && bp === currentBp) {
+    return true;
+  }
+
+  // the current breakpoint isn’t the one you’re looking for
+  return false;
+};
+
 var jsonpRequest = function jsonpRequest(settings) {
   // Doc: https://code.area17.com/a17/a17-helpers/wikis/jsonpRequest
 
@@ -1356,6 +1414,7 @@ var a17helpers = {
   getMetaContentByName: getMetaContentByName,
   getOffset: getOffset,
   getUrlParameterByName: getUrlParameterByName,
+  isBreakpoint: isBreakpoint,
   jsonpRequest: jsonpRequest,
   keycodes: keyCodes,
   lazyLoad: lazyLoad,
