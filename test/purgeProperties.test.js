20a5afaa3e7d3f864afd7c448f3cf43b966de71b
Kyle Goines <kylegoines@Kyles-MBP.home>
2020-01-15 15:16:54 -0500

    adding purgeprops tests

---
+++ test/purgeProperties.test.js
@@ -0,0 +1,17 @@
+import purgeProperties from './../src/utility/purgeProperties';
+
+describe('purge props', () => {
+  it('exists', () => {
+    expect(typeof purgeProperties).toBe('function');
+  });
+
+  it('purges all events', () => {
+    const objectthing = {
+      thing: 'all',
+      somethingelse: 1234,
+      nested: { name: [123, 123] },
+    };
+    purgeProperties(objectthing);
+    expect(objectthing).toEqual({});
+  });
+});
