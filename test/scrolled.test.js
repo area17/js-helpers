6282799fae83494b5506d9f5627bae2a89595688
Kyle Goines <kylegoines@Kyles-MacBook-Pro.local>
2020-01-16 10:25:42 -0500

    scrolled test

---
+++ test/scrolled.test.js
@@ -0,0 +1,29 @@
+import scrolled from './../src/utility/scrolled';
+const realDispatchEvent = document.dispatchEvent;
+document.dispatchEvent = jest.fn();
+
+beforeEach(() => {
+  jest.spyOn(window, 'requestAnimationFrame').mockImplementation((cb) => cb());
+});
+
+afterEach(() => {
+  window.requestAnimationFrame.mockRestore();
+});
+
+window.pageYOffset = jest
+  .fn(() => 0)
+  .mockImplementationOnce(() => 0)
+  .mockImplementationOnce(() => 100);
+
+describe('scrolled', () => {
+  it('exists', () => {
+    expect(typeof scrolled).toBe('function');
+  });
+
+  it('triggers scroll on dispatch', () => {
+    scrolled();
+    window.dispatchEvent(new Event('scroll'));
+
+    expect(document.dispatchEvent.mock.calls[0][0].type).toBe('scrolled');
+  });
+});
