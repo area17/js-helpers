d10ab71c4b35021b2204bb5635b15c09d4635687
Mike Byrne <mike@area17.com>
2020-05-27 16:53:28 +0100

    read breakpoint from CSS variable

--- src/utility/getCurrentMediaQuery.js
+++ src/utility/getCurrentMediaQuery.js
@@ -1,17 +1,7 @@
 var getCurrentMediaQuery = function() {
   // Doc: https://code.area17.com/a17/a17-helpers/wikis/getCurrentMediaQuery
 
-  function parse(str) {
-    return str.replace(/'/gi, '').replace(/"/gi, '');
-  }
-
-  if (window.opera) {
-    return parse(window.getComputedStyle(document.body, ':after').getPropertyValue('content')) || 'large';
-  } else if (window.getComputedStyle) {
-    return parse(window.getComputedStyle(document.head, null).getPropertyValue('font-family')) || 'large';
-  } else {
-    return 'large';
-  }
+  return getComputedStyle(document.documentElement).getPropertyValue('--breakpoint');
 };
 
 
