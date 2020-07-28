d9da5b51893292121e02759af1ec33f8a9d3f5b1
Mike Byrne <mike@area17.com>
2020-07-28 12:17:43 +0100

    trim output of reading of `--breakpoint` root var
    Otherwise it would output strings like ' xlarge' 🤦‍♂️

--- src/utility/getCurrentMediaQuery.js
+++ src/utility/getCurrentMediaQuery.js
@@ -1,7 +1,7 @@
 var getCurrentMediaQuery = function() {
   // Doc: https://code.area17.com/a17/a17-helpers/wikis/getCurrentMediaQuery
 
-  return getComputedStyle(document.documentElement).getPropertyValue('--breakpoint');
+  return getComputedStyle(document.documentElement).getPropertyValue('--breakpoint').trim();
 };
 
 
