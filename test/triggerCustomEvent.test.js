b8358c90215320bddf1acbdaa50e1f18787cb867
Antoine Doury <antoine@area17.com>
2021-05-24 22:04:03 +0200

    Remove outdated test

--- test/triggerCustomEvent.test.js
+++
@@ -1,8 +0,0 @@
-import triggerCustomEvent from './../src/utility/triggerCustomEvent';
-
-// depricated
-describe('custom event utility', () => {
-  it('exists', () => {
-    expect(typeof triggerCustomEvent).toBe('function');
-  });
-});
