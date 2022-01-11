6489a75c1c2574c1e10498c30d1f4b8ea6e5af50
Mike Byrne <mike@area17.com>
2022-01-11 11:59:41 +0000

    update exportd

--- src/index.js
+++ src/index.js
@@ -1,63 +1,30 @@
-import ajaxRequest from './ajaxRequest.js';
-import cookieHandler from './cookieHandler.js';
-import copyTextToClipboard from './copyTextToClipboard';
-import debounce from './debounce';
-import escapeString from './escapeString.js';
-import extend from './extend.js';
-import focusDisplayHandler from './focusDisplayHandler';
-import focusTrap from './focusTrap';
-import fontLoadObserver from './fontLoadObserver';
-import getCurrentMediaQuery from './getCurrentMediaQuery.js';
-import getIndex from './getIndex.js';
-import getMetaContentByName from './getMetaContentByName.js';
-import getOffset from './getOffset.js';
-import getUrlParameterByName from './getUrlParameterByName.js';
-import ios100vhFix from './ios100vhFix';
-import isBreakpoint from './isBreakpoint.js';
-import jsonpRequest from './jsonpRequest.js';
-import keycodes from './keycodes.js';
-import lazyLoad from './lazyLoad';
-import messages from './messages.js';
-import objectifyForm from './objectifyForm.js';
-import oritentationChangeFix from './oritentationChangeFix.js';
-import purgeProperties from './purgeProperties.js';
-import queryStringHandler from './queryStringHandler';
-import resized from './resized.js';
-import responsiveImageUpdate from './responsiveImageUpdate';
-import scrolled from './scrolled.js';
-import sendEventToSegmentio from './sendEventToSegmentio.js';
-import setFocusOnTarget from './setFocusOnTarget';
-import Store from './store';
-
-export {
-  ajaxRequest,
-  cookieHandler,
-  copyTextToClipboard,
-  debounce,
-  escapeString,
-  extend,
-  focusDisplayHandler,
-  focusTrap,
-  fontLoadObserver,
-  getCurrentMediaQuery,
-  getIndex,
-  getMetaContentByName,
-  getOffset,
-  getUrlParameterByName,
-  ios100vhFix,
-  isBreakpoint,
-  jsonpRequest,
-  keycodes,
-  lazyLoad,
-  messages,
-  objectifyForm,
-  oritentationChangeFix,
-  purgeProperties,
-  queryStringHandler,
-  resized,
-  responsiveImageUpdate,
-  scrolled,
-  sendEventToSegmentio,
-  setFocusOnTarget,
-  Store
-};
+export { defailt as ajaxRequest } from './ajaxRequest.js';
+export { defailt as cookieHandler } from './cookieHandler.js';
+export { defailt as copyTextToClipboard } from './copyTextToClipboard';
+export { defailt as debounce } from './debounce';
+export { defailt as escapeString } from './escapeString.js';
+export { defailt as extend } from './extend.js';
+export { defailt as focusDisplayHandler } from './focusDisplayHandler';
+export { defailt as focusTrap } from './focusTrap';
+export { defailt as fontLoadObserver } from './fontLoadObserver';
+export { defailt as getCurrentMediaQuery } from './getCurrentMediaQuery.js';
+export { defailt as getIndex } from './getIndex.js';
+export { defailt as getMetaContentByName } from './getMetaContentByName.js';
+export { defailt as getOffset } from './getOffset.js';
+export { defailt as getUrlParameterByName } from './getUrlParameterByName.js';
+export { defailt as ios100vhFix } from './ios100vhFix';
+export { defailt as isBreakpoint } from './isBreakpoint.js';
+export { defailt as jsonpRequest } from './jsonpRequest.js';
+export { defailt as keycodes } from './keycodes.js';
+export { defailt as lazyLoad } from './lazyLoad';
+export { defailt as messages } from './messages.js';
+export { defailt as objectifyForm } from './objectifyForm.js';
+export { defailt as oritentationChangeFix } from './oritentationChangeFix.js';
+export { defailt as purgeProperties } from './purgeProperties.js';
+export { defailt as queryStringHandler } from './queryStringHandler';
+export { defailt as resized } from './resized.js';
+export { defailt as responsiveImageUpdate } from './responsiveImageUpdate';
+export { defailt as scrolled } from './scrolled.js';
+export { defailt as sendEventToSegmentio } from './sendEventToSegmentio.js';
+export { defailt as setFocusOnTarget } from './setFocusOnTarget';
+export { defailt as Store } from './store';
