2215dd6865f59e9977727d415fa2cbf74f052f4c
Mike Byrne <mike@area17.com>
2022-01-24 16:05:32 +0000

    add `todo` tests for missing tests

---
+++ test/ajaxRequest.test.js
@@ -0,0 +1,17 @@
+import ajaxRequest from './../src/ajaxRequest';
+
+describe('ajaxRequest', () => {
+  it('exists', () => {
+    expect(typeof ajaxRequest).toBe('function');
+  });
+
+  it.todo('Sends XMLHttpRequest get requests');
+
+  it.todo('Sends XMLHttpRequest post requests');
+
+  it.todo('Run success callback on success');
+
+  it.todo('Run error callback on success');
+
+  it.todo('Aborts requests');
+});
