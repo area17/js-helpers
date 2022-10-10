f7f3b25ec2e2fb2126f9b3f4603f4ae271ca57b4
Antoine <antoine@area17.com>
2022-10-10 15:51:56 +0200

    Update ios100vhFix : get the greater of document.documentElement.clientHeight versus window.innerHeight to fix incorrect height when the browser UI is hiding on iOS15 and above.

--- src/ios100vhFix.js
+++ src/ios100vhFix.js
@@ -2,7 +2,7 @@ export function ios100vhFix() {
   // Doc: https://github.com/area17/a17-behaviors/wiki/ios100vhFix
 
   function setVh() {
-    const vh = document.documentElement.clientHeight * 0.01;
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   }
 
