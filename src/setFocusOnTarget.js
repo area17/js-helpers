7c33545b44084ae5f0d2f7c5ba9b834d733af942
Mike Byrne <mike@area17.com>
2023-03-06 19:09:59 +0000

    update `setFocusOnTarget`
    has a new "exact" mode which can select the exact
    node or attempt to focus the first focusable element
    inside of the node
    
    it also prevents the page from scrolling to the focused
    item - so you can manually control this

--- src/setFocusOnTarget.js
+++ src/setFocusOnTarget.js
@@ -1,11 +1,29 @@
-var setFocusOnTarget = function(node) {
+import getFocusableElements from './getFocusableElements';
+
+var setFocusOnTarget = function($node, exact = true) {
   // Doc: https://github.com/area17/a17-behaviors/wiki/setFocusOnTarget
-  node.focus();
-  if (node!== document.activeElement) {
-    node.setAttribute('tabindex','-1');
-    node.focus();
-    node.removeAttribute('tabindex');
+
+  if (!($node instanceof Element) && !($node instanceof HTMLDocument)) {
+    return;
   }
-};
+
+  if (exact) {
+    $node.focus();
+    if (document.activeElement !== $node) {
+      $node.setAttribute('tabindex','-1');
+      $node.focus({ preventScroll: true });
+      $node.removeAttribute('tabindex');
+    }
+  } else {
+    const $focusable = getFocusableElements($node, true)[0] || $node;
+    if ($focusable) {
+      $focusable.focus({ preventScroll: true });
+    } else {
+      $node.setAttribute('tabindex','-1');
+      $node.focus({ preventScroll: true });
+      $node.removeAttribute('tabindex');
+    }
+  }
+}
 
 export default setFocusOnTarget;
