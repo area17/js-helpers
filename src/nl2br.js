ea23d13c6471cfb22243ec70e6d9b72bed3df0d4
Mike Byrne <mike@area17.com>
2023-03-07 17:47:24 +0000

    added Nike's nl2br helper
    https://code.area17.com/nike/nike-inc-next/-/blob/production/src/utils/nl2br.ts

---
+++ src/nl2br.js
@@ -0,0 +1,16 @@
+/**
+ * This function is same as PHP's nl2br() with default parameters.
+ *
+ * @param {string} str Input text
+ * @param {boolean} replaceMode Use replace instead of insert
+ * @param {boolean} isXhtml Use XHTML
+ * @return {string} Filtered text
+ */
+
+const nl2br = function(str: string, replaceMode: boolean = false) {
+  const breakTag = '<br>';
+  const replaceStr = replaceMode ? '$1' + breakTag : '$1' + breakTag + '$2';
+  return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, replaceStr);
+}
+
+export default nl2br;
