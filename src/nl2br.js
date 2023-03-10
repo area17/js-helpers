79e43e6c81fced8a64592c674a62a84561813e3d
Mike Byrne <mike@area17.com>
2023-03-10 17:27:43 +0000

    updates `nl2br` to note different to PHP `nl2br`

--- src/nl2br.js
+++ src/nl2br.js
@@ -1,16 +1,22 @@
 /**
- * This function is same as PHP's nl2br() with default parameters.
+ * This function is similar to PHP's nl2br()
  *
  * @param {string} str Input text
  * @param {boolean} replaceMode Use replace instead of insert
- * @param {boolean} isXhtml Use XHTML
  * @return {string} Filtered text
  */
 
-const nl2br = function(str, replaceMode = false) {
-  const breakTag = '<br>';
-  const replaceStr = replaceMode ? '$1' + breakTag : '$1' + breakTag + '$2';
-  return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, replaceStr);
+const nl2br = function(str, replaceMode = true) {
+  // Doc: https://github.com/area17/a17-behaviors/wiki/nl2br
+
+  if (typeof str === 'string') {
+    const breakTag = '<br>';
+    const replaceStr = replaceMode ? '$1' + breakTag : '$1' + breakTag + '$2';
+    return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, replaceStr);
+  } else {
+    console.log('Warning - nl2br - no string passed');
+    return '';
+  }
 }
 
 export default nl2br;
