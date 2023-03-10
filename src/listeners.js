7246579a230ea38118b0f47d6741bb3eb42085d0
Mike Byrne <mike@area17.com>
2023-03-10 17:28:16 +0000

    `listeners` updated to have `listeners.add` and `listeners.remove`

--- src/listeners.js
+++ src/listeners.js
@@ -1,13 +1,27 @@
-const addListener = (nodes, func, opt = false) => {
-  nodes.forEach($node => {
-    $node.addEventListener('click', func, opt);
-  });
+const listeners = {
+  add(nodes, func, options = false) {
+    // Doc: https://github.com/area17/a17-behaviors/wiki/addListener
+
+    if (NodeList.prototype.isPrototypeOf(nodes)) {
+      nodes.forEach($node => {
+        $node.addEventListener('click', func, options);
+      });
+    } else {
+      console.log('Warning - addListener - no NodeList passed');
+      return '';
+    }
+  },
+  remove(nodes, func, options = false) {
+    // Doc: https://github.com/area17/a17-behaviors/wiki/removeListener
+    if (NodeList.prototype.isPrototypeOf(nodes)) {
+      nodes.forEach($node => {
+        $node.removeEventListener('click', func, options);
+      });
+    } else {
+      console.log('Warning - addListener - no NodeList passed');
+      return '';
+    }
+  },
 };
 
-const removeListener = (nodes, func, opt) => {
-  nodes.forEach($node => {
-    $node.removeEventListener('click', func, opt);
-  });
-};
-
-export { addListener, removeListener };
+export default listeners;
